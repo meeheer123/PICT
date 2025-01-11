62ba5ff75114e6c1c9a217bc9728337482a1572a
Mihir <mihir.pande22@pccoepune.org>
2025-01-12 00:08:52 +0530

    tried adding image

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -18,13 +18,7 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: '/icon-512x512.png',
+            src: 'Frontend/public/location.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
